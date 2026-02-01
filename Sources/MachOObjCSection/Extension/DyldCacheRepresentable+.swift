4ba7e50bfdc4ad4da63693509443bb6ac34c8e29
lynnswap <65545348+lynnswap@users.noreply.github.com>
2026-02-01 21:50:10 +0900

    Restore old objc optimization offset access

--- Sources/MachOObjCSection/Extension/DyldCacheRepresentable+.swift
+++ Sources/MachOObjCSection/Extension/DyldCacheRepresentable+.swift
@@ -12,15 +12,10 @@ import MachOKit
 extension DyldCacheRepresentable {
     var relativeMethodSelectorBaseAddressOffset: UInt64? {
         if let objcOpt = objcOptimization {
-            return numericCast(objcOpt.layout.relativeMethodSelectorBaseAddressOffset)
+            return objcOpt.relativeMethodSelectorBaseAddressOffset
         }
         if let oldOpt = oldObjcOptimization {
-            switch oldOpt {
-            case .v16(let optimization):
-                return numericCast(optimization.layout.relativeMethodSelectorBaseAddressOffset) + numericCast(optimization.offset)
-            default:
-                return nil
-            }
+            return numericCast(oldOpt.relativeMethodSelectorBaseAddressOffset) + numericCast(oldOpt.offset)
         }
         return nil
     }
